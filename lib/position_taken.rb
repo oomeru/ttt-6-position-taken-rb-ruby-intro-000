37e0b584c95f9fce629e5ed6ef9d15b7f6770535
oomeru <obaroomeru@gmail.com>
2017-08-26 16:22:26 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
 #  if board[index] == " " || "" || nil
 #    return false
 #  end
-  if "X" || "O" == board[index]
+  if board[index] == "X" || board[index] == "O"
     return true
   end
 end
